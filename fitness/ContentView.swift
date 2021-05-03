2465b63b605723efb60e620985cea48c2b4d0e02
Bryan Ortiz <ortiz.b.t@gmail.com>
2021-05-03 11:47:53 -0500

    first push

---
+++ fitness/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  fitness
+//
+//  Created by Bryan Ortiz on 5/2/21.
+//  Copyright © 2021 Ortiz. All rights reserved.
+//
+
+import SwiftUI
+import Alamofire
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+            Button(action: doSomething) {
+                Text("Click Me")
+            }
+        }
+    }
+    
+    private let doSomething: () -> Void = {
+        let headers: HTTPHeaders = [
+            HTTPHeader.authorization("Token ")
+        ]
+        AF.request("https://wger.de/api/v2/userprofile/", method: .get, headers: headers)
+            .responseJSON { response in
+                debugPrint(response)
+        }
+        return
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
